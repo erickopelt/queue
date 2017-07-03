6a19db09cdc306c23577c22e3fcda7d2a880e4b0
erickeuller <erick.opelt@univille.edu.br>
2017-07-03 05:59:44 -0300

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="hello">
+    <div class="enqueue">
+      <input type="number" v-model="enqueueValue" name="" value="" class="queue-input form-input">
+      <br>
+      <button type="button"
+      v-on:click="enqueue(enqueueValue)" class="btn">Enqueue</button>
+    </div>
+    <div class="dequeue">
+      <button type="button" v-on:click="dequeue()" name="button" class="btn">Dequeue</button>
+    </div>
+    <ul class="queue">
+      <transition-group
+        enter-active-class="fadeInLeft"
+        leave-active-class="fadeOutRight"
+        tag="ul">
+        <li v-for="item in queue" v-bind:key="item.id" class="queue-item">
+          <span class="item-number">{{item.value}}</span>
+        </li>
+      </transition-group>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      queue: [],
+      enqueueValue: 0
+    }
+  },
+  methods: {
+    enqueue: function (value) {
+      return this.queue.unshift({
+        value: value,
+        id: Math.random()
+      })
+    },
+    dequeue: function () {
+      return this.queue.pop()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+.queue-input{
+  width: 80px;
+  height: 80px;
+  margin: 10px;
+  padding: 10px;
+}
+
+.dequeue {
+  margin-top: 10px;
+}
+
+.queue-item {
+  width: 40px;
+  height: 40px;
+  border: 1px solid grey;
+  position: relative;
+}
+
+.item-number {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.queue {
+  margin: 30px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+require('vue2-animate/dist/vue2-animate.min.css')
+
+Vue.use(BootstrapVue)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
